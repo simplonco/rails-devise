22a7eeb069c055f1ab04c8d9b35f6386bf6f747c
Andrei Vladescu-Olt <andrei.volt@gmail.com>
2014-02-06 18:18:45 +0100

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsDevise::Application.config.secret_key_base = 'e12f0352ed2e27817c0507dc3c22f6645dc16772eddff9dceadceb13bff15b9d1b7f6dae7eaf51426ca7ef0d50ec76b86d469e9eb08d9f3740df384bcb1dc984'
